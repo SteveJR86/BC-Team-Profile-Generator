358bfea26f672af3cdb0c0c7839f21f186a08596
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2023-03-04 17:02:54 +0000

    menu system is looping and engineer can be added to team in addition to the manager with html generated

--- index.js
+++ index.js
@@ -10,6 +10,109 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// const to hold team as array
+const team = [];
+
+// function to call the choices
+function makeChoice(){
+  const choices = [
+    {
+      type: "list",
+      name: "choice",
+      message: "What do you want to do next?",
+      choices: ["Add an engineer", "Add an intern", "Finish building the team"],
+    }
+  ]
+
+  inquirer.prompt(choices)
+  .then(choice => {
+    console.log(choice);
+    if(choice.choice === "Add an engineer") {
+      addEngineer();
+    } else if (choice.choice === "Add an intern"){
+      addIntern();
+    } else {
+      renderHTML();
+    }
+  }
+  )
+}
+
+// function to add an engineer to team
+function addEngineer(){
+  const questions = [
+    {
+    type: "input",
+    name: "name",
+    message: "What is the Engineers name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the Engineers id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the Engineers email?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is the Engineers GitHub Username?",
+    },
+  ]
+  inquirer.prompt(questions)
+  .then(answers => {
+    const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    team.push(engineer);
+    makeChoice();
+  })
+}
+// function to add an intern to team
+function addIntern(){
+  console.log("adding new intern");
+  makeChoice();
+}
+// function to render team to HTML
+function renderHTML(){
+  const htmlContent = render(team);
+  fs.writeFile("test.html", htmlContent, function(err) {
+    if(err) throw err;
+    console.log("Saved!");
+  });
+}
+  
+
+// ask manager questions
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the team managers name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is their Employee ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is their email address?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is their office number?",
+  },
+]
+
+inquirer.prompt(managerQuestions)
+.then(answers => {
+  const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+  team.push(manager);
+  makeChoice();
+});
